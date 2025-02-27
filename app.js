4a0f4ab5c64307e2cb432d8833e54e4769e9f040
pawel <pawel@gmail.com>
2025-02-27 14:31:22 +0100

    11 commit ( teścik )

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import AnimalsController from './zookeeperii/controllers/AnimalsController.js'
+
+const app = express()
+const port = process.env.PORT || 3000
+app.use(express.json())
+
+app.get("/", (req,res) =>{
+    res.send("ok")
+})
+
+app.get("/animals", AnimalsController.getAllAnimals)
+app.get("/animals/:id", AnimalsController.getAnimalById)
+app.delete("/animals/:id", AnimalsController.deleteAnimal)
+app.put("/animals/put/:id", AnimalsController.updateAnimal)
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
