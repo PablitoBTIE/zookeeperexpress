a0adb0d868298707ed72125d04c8900dd26f86cf
PablitoBTIE <pawel.mierzejewskib@cyfroweszkoly.pl>
2025-03-02 12:28:57 +0100

    drobne poprawki

--- zookeeperii/controllers/AnimalsController.ts
+++ zookeeperii/controllers/AnimalsController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import AnimalsServices from "../services/animalsServices";
 
 const AnimalsController = {
+    getAnimalById: undefined,
 
     async deleteAnimal(req: Request, res: Response): Promise<void> {
         const animalId: number = parseInt(req.params.id);
@@ -30,7 +31,7 @@ const AnimalsController = {
 
         try {
             const addedAnimal = await AnimalsServices.addAnimal(newAnimal);
-            res.status(201).json(addedAnimal); // Return the added animal
+            res.status(200).json(addedAnimal);
         } catch (err) {
             console.error(err);
             res.status(500).json({ message: "Failed to add animal" });
@@ -40,7 +41,7 @@ const AnimalsController = {
 
     async updateAnimal(req: Request, res: Response): Promise<void> {
         const animalId: number = parseInt(req.params.id);
-        const newAnimal: any = req.body;  // you can replace `any` with a specific type for the animal object
+        const newAnimal: any = req.body;
 
         try {
             const updatedAnimal = await AnimalsServices.updateAnimal(animalId, newAnimal);
